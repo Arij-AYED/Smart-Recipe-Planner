e7a3bee137f5c9eb82e6089cc3ea82fad3782e37
Arij AYED <arijayed9@gmail.com>
2025-07-17 13:42:56 +0100

    fixed changes

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,17 @@ require ('dotenv').config();
 const express = require('express');
 const mongoose = require ('mongoose');
 const authRoutes = require ('./routes/auth');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 //Connect to mongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err)=>console.error('MongoDB error:',err));
+
 
 //use auth routes
 app.use('/api',authRoutes);
