c1710bd2be0da9ac53019e5b3595c69018e6d0e2
azizzahra <aziz.zahra@esprit.tn>
2025-07-18 09:33:33 +0100

    .

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,18 @@
+require ('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const recipeRoutes = require('./routes/recipes');
+const authRoutes = require ('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/recipeDB';
-
-
-// Middleware
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/recipeDB', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -23,8 +21,10 @@ mongoose.connect('mongodb://localhost:27017/recipeDB', {
 
 // Routes
 app.use('/recipes', recipeRoutes);
+app.use('/api',authRoutes);
 
 // Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
